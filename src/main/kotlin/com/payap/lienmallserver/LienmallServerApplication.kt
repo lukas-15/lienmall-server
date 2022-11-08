59ee17a3aa0d73f6c7d5a484c8f7e8e62516b11d
KOSEUNGBIN <kosb15@naver.com>
2022-11-08 16:33:22 +0900

    Init project settings

---
+++ src/main/kotlin/com/payap/lienmallserver/LienmallServerApplication.kt
@@ -0,0 +1,11 @@
+package com.payap.lienmallserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LienmallServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<LienmallServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/payap/lienmallserver/LienmallServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.payap.lienmallserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LienmallServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
